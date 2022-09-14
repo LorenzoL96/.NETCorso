6d130f99860f80a2eb01581b21da121738a6e0d3
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-09-14 12:57:34 +0200

    Aggiunto ASP.NET Core MVC

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,8 @@ namespace prova
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,11 +28,11 @@ namespace prova
             }
             app.UseStaticFiles(); //middleware per gestione file statici (immagini, html, css....)
 
-            app.Run(async (context) =>
-            {
-                
-                await context.Response.WriteAsync($"Hello");
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder => {
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+            
         }
     }
 }
