ab970425795546d32cc084af23ea92c003ecfed5
Lorenzo Lorusso <lorenzo.gml@gmail.com>
2022-09-15 14:35:18 +0200

    Impostati file di layout e barra di navigazione

--- obj/Debug/netcoreapp2.2/Razor/Views/Courses/Detail.cshtml.g.cs
+++ obj/Debug/netcoreapp2.2/Razor/Views/Courses/Detail.cshtml.g.cs
@@ -14,6 +14,7 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c3500add4d97201d45d8b1fa50252b1787534df9", @"/Views/Courses/Detail.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c66e443ba81bfd444e2b1c1ae94c4deedf2b8d44", @"/Views/_ViewImports.cshtml")]
     public class Views_Courses_Detail : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
